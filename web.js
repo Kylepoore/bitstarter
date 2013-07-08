8c2e8cbe3cf417c91654f67aadf38b969f7d6d61
Kyle Poore <kylepoore1@gmail.com>
2013-07-08 18:52:38 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  buff = fs.readFileSync('index.html');
+  response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
